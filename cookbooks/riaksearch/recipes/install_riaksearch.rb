178ccf8f9221596ae5aca090efc40643a0a86a6e
Scott M. Likens <slikens@engineyard.com>
2011-01-24 17:05:25 -0800

    - Re-implement riaksearch with 0.14
      Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- cookbooks/riaksearch/recipes/install_riaksearch.rb
+++ cookbooks/riaksearch/recipes/install_riaksearch.rb
@@ -2,7 +2,7 @@
 #
 
 gem_package "ripple" do
-  version "0.8.2" 
+  version "0.9.0.beta"
   action :install
 end
 
@@ -11,18 +11,18 @@ gem_package "curb" do
   action :install
 end
 
-remote_file "/mnt/src/riak_search-0.13.0.tar.gz" do
-  source "http://downloads.basho.com/riak-search/riak-search-0.13/riak_search-0.13.0.tar.gz"
+remote_file "/mnt/src/riak_search-0.14.0-1.tar.gz" do
+  source "http://downloads.basho.com/riak-search/riak-search-0.14/riak_search-0.14.0-1.tar.gz"
   backup 0
-  not_if { FileTest.exists?("/mnt/src/riak_search-0.13.0.tar.gz") }
+  not_if { FileTest.exists?("/mnt/src/riak_search-0.14.0-1.tar.gz") }
 end
 
 execute "untar riak" do
-  command "cd /mnt/src;tar zxf riak_search-0.13.0.tar.gz"
-  not_if { FileTest.directory?("/mnt/src/riak_search-0.13.0") }
+  command "cd /mnt/src;tar zxf riak_search-0.14.0-1.tar.gz"
+  not_if { FileTest.directory?("/mnt/src/riak_search-0.14.0") }
 end
 
 execute "make all rel" do
-  command "cd /mnt/src/riak_search-0.13.0;make all rel && cp -prv rel/riaksearch /data/riaksearch"
+  command "cd /mnt/src/riak_search-0.14.0;make all rel && cp -prv rel/riaksearch /data/riaksearch"
   not_if { FileTest.exists?("/data/riaksearch/bin/riaksearch") }
 end
