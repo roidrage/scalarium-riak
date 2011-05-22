f0110d388bd6fedf34126e4a62378318bf7d0edd
Scott M. Likens <slikens@engineyard.com>
2011-05-22 15:02:49 -0700

    - Put Erlang R130B4 on the app instances as well.
    Signed-off-by: Scott M. Likens <slikens@engineyard.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,18 @@
 # This portion of the recipe runs on all instances, except the utility instances.  You don't want haproxy on the utility instances but you do on solo|app|app_master
 if ['solo','app','app_master'].include?(node[:instance_role])
   require_recipe "riak::haproxy"
-#  require_recipe "riak::basho_bench"
+  require_recipe "riak::basho_bench"
+  # Remove old version of Erlang that is NOT new enough
+  package "dev-lang/erlang" do
+    version "erlang-12.2.5-r1"
+    action :remove
+    only_if { FileTest.directory?("/var/db/pkg/dev-lang/erlang-12.2.5-r1") }
+  end
+
+# Install a version of Erlang (R130B4) provided by me.  I built the package, if you would like to see the ebuild sources make an issue.
+  s3_install "dev-lang/erlang" do
+    version "13.2.4"
+  end
 end
 
 # This portion of the recipe runs on only utility instances
@@ -39,4 +50,3 @@ if ['util'].include?(node[:instance_role])
     #include_recipe "riak::basho_bench"
   end
 end
-
