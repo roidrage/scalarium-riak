e769873a0d9831606b1513e214fc38215e0925ca
Mathias Meyer <meyer@paperplanes.de>
2011-06-15 16:25:54 +0200

    Include our custom haproxy recipe.
    This included the default one.

--- riak/recipes/default.rb
+++ riak/recipes/default.rb
@@ -1,5 +1,5 @@
-include_recipe "haproxy"
 include_recipe "riak::install_riak"
 include_recipe "riak::configure_riak"
 include_recipe "riak::start_riak"
 include_recipe "riak::join_ring"
+include_recipe "riak::haproxy"
