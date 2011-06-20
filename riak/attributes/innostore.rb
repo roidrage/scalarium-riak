0cb32f5935db1f43db1f397c91e9fd9762d5e245
Mathias Meyer <meyer@paperplanes.de>
2011-06-20 09:09:29 +0200

    Always set innostore options, enable recipe.

--- riak/attributes/innostore.rb
+++ riak/attributes/innostore.rb
@@ -1,12 +1,9 @@
-if node[:riak][:storage_backend] == "riak_kv_innostore_backend"
-  innostore :log_buffer_size => 8388608,
-            :log_files_in_group => 8,
-            :log_file_size => 1073741824, # 1 GB default
-            :flush_log_at_trx_commit => 1,
-            :data_home_dir => "#{node[:riak][:data_dir]}/innodb",
-            :log_group_home_dir = "#{node[:riak][:data_dir]}/innodb",
-            :buffer_pool_size => 2147483648,
-            :version => "1.0.2",
-            :flush_method => "O_DIRECT"
-end
- 
+innostore :log_buffer_size => 8388608,
+          :log_files_in_group => 8,
+          :log_file_size => 1073741824, # 1 GB default
+          :flush_log_at_trx_commit => 1,
+          :data_home_dir => "#{node[:riak][:data_dir]}/innodb",
+          :log_group_home_dir = "#{node[:riak][:data_dir]}/innodb",
+          :buffer_pool_size => 2147483648,
+          :version => "1.0.2",
+          :flush_method => "O_DIRECT"
--- riak/recipes/default.rb
+++ riak/recipes/default.rb
@@ -1,4 +1,7 @@
 include_recipe "riak::install_riak"
+if node[:riak][:storage_backend] == "riak_kv_innostore_backend"
+  include_recipe "riak::innostore"
+end
 include_recipe "riak::configure_riak"
 include_recipe "riak::start_riak"
 include_recipe "riak::join_ring"
