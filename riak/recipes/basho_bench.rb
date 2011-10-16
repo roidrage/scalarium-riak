c8f4463db154b0b96dd961d32571e922a7710c9d
Mathias Meyer <meyer@paperplanes.de>
2011-10-16 17:17:29 +0200

    Install getopt for R too.

--- riak/recipes/basho_bench.rb
+++ riak/recipes/basho_bench.rb
@@ -4,12 +4,9 @@ execute "apt-add-repository 'deb http://ftp.heanet.ie/mirrors/cran.r-project.org
 
 execute "apt-get update"
 
-package "r-base" do
-  action :install
-  options "--force-yes"
-end
-
-package "r-base-dev" do
-  action :install
-  options "--force-yes"
+%w{r-base r-base-dev r-cran-getopt}.each do |package|
+  package package do
+    action :install
+    options "--force-yes"
+  end
 end
