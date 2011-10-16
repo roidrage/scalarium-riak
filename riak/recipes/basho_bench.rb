9d0c2d0c45033d4171f7489604fb13b317433b60
Mathias Meyer <meyer@paperplanes.de>
2011-10-16 13:38:30 +0200

    Use a real mirror.

--- riak/recipes/basho_bench.rb
+++ riak/recipes/basho_bench.rb
@@ -1,6 +1,6 @@
 # AppCloud's GCC does not include Fortran by default, so let's install GCC binaries with fortran.
 
-execute "apt-add-repository 'deb http://<my.favorite.cran.mirror>/bin/linux/ubuntu natty/'"
+execute "apt-add-repository 'deb http://ftp.heanet.ie/mirrors/cran.r-project.org/bin/linux/ubuntu/ natty/'"
 
 execute "apt-get update"
 
